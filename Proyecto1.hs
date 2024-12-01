93092e84e3c18cf9f4bb813046008148d8877ebc
César Carios <cesarcariosbrancovic@gmail.com>
2024-12-01 17:10:18 -0400

    Primera parte funcional: se inicializa la matriz y se imprime en un formato legible
    Co-authored-by: JhonatanHZ <JhonatanHZ@users.noreply.github.com>

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -1,40 +1,33 @@
--- César Carios, C.I: 30136117 --
--- Jhonatan Homsany, C.I: 30182893 --
+-- Parte 1: Inicializacion y visualizacion de la matriz --
 
-{-
+fillArray :: Int -> Int -> Int -> Int -> [Int]
+fillArray n g i c
+  | c == n  = []
+  | c == i  = g : fillArray n g i (c + 1)
+  | otherwise = 0 : fillArray n g i (c + 1)
 
-mifun :: [[Int]] -> Int
-mifun = foldr op 0
-    where op x r = head x + r
+initMatrix :: Int -> Int -> (Int, Int) -> [[Int]]
+initMatrix n g (x, y) = [fillRow i | i <- [0..n-1]]
+  where
+    fillRow i
+      | i == x    = fillArray n g y 0
+      | otherwise = fillArray n 0 y 0
 
-main :: IO ()
-main = do
-    let listas = [[1,2,3] , [2,4,5]]
-    let resultado = mifun listas
-    putStrLn $ "La suma de los primeros elementos es: " ++ show resultado
-    
-    -}
 
--- Parte 1: Inicialización y visualización de la matriz -- 
+instance {-# OVERLAPPING #-} Show [[Int]] where
+    show :: [[Int]] -> String
+    show [] = ""
+    show (row:rows) = showRow row ++ "\n" ++ show rows
+        where
+            showRow [] = ""
+            showRow [x] = show x
+            showRow (x:xs) = show x ++ " " ++ showRow xs
 
-initMatrix :: Int -> Int -> (Int, Int) -> [[ Int ]]
-initMatrix n g (x,y) = [[if i == x && j == y then g else 0 
-    | j <- [0..n-1]] 
-    | i <- [0..n-1]]
-
-matrixString :: [[Int]] -> String
-matrixString = unlines . map(unwords . map show)
-
-printMatrix :: [[Int]] -> IO ()
-printMatrix = putStr . matrixString
 
 -- Parte 2: Logica de colapso
 
-collapseStep :: [[ Int ]] -> [[ Int ]]
-collapseStep (x:xs) 
-
 main :: IO ()
 main = do
-    
-    let matrix = initMatrix 3 20 (1,1)
-    printMatrix matrix
+
+    let matrix = initMatrix 3 20 (1, 1)
+    print matrix
--- README.md
+++ README.md
@@ -2,8 +2,3 @@
 
 First proyect for the subject Programming Languages
 
-# Funciones que fueron útiles
-
-* Unlines: Convierte un arreglo en un string e inserta una nueva línea entre los elementos del string.
-
-* Unwords: Convierte un arreglo en un string y separa sus elementos con espacios en blanco.
